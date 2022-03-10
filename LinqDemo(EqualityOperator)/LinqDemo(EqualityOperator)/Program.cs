8b51887f97b7bc0adb9da55560fb2bfe5d10d48a
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-10 15:26:08 +0530

    10-03-2022

--- LinqDemo(EqualityOperator)/LinqDemo(EqualityOperator)/Program.cs
+++ LinqDemo(EqualityOperator)/LinqDemo(EqualityOperator)/Program.cs
@@ -14,7 +14,6 @@ namespace LinqDemo_EqualityOperator_
             IList<string> list2 = new List<string>() { "one", "two", "three", "four", "five" };
             bool isEqual = list1.SequenceEqual(list2);
             Console.WriteLine(isEqual);
-            //---My Name is Anshuman
         }
     }
 }
