d39bdf620fdc55c7d19a01aad17c0d7c4e1f3877
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-10 16:49:18 +0530

    10-03-2022

---
+++ LinqDemo(Genertation Operator)/LinqDemo(Genertation Operator)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo_Genertation_Operator_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //---Empty Method
+            var emptyCollection1 = Enumerable.Empty<string>();
+            var emptyCollection2 = Enumerable.Empty<Student>();
+            Console.WriteLine("Type" + emptyCollection1.GetType().Name);
+            Console.WriteLine("Count" + emptyCollection2.Count());
+
+            //---Range Method
+            var intCollecttion = Enumerable.Range(10, 10);
+            Console.WriteLine("Total count: " + intCollecttion.Count());
+
+            for (int i = 0; i < intCollecttion.Count(); i++)
+            {
+                Console.WriteLine("Value at index " + intCollecttion.ElementAt(i));
+            }
+            Console.Read();
+
+        }
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        
+    }
+}
