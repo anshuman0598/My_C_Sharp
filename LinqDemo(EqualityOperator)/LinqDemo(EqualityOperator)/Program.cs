da4580029df17ffb2269580e3c0224c7644518c4
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-10 15:23:38 +0530

    10-03-2022

---
+++ LinqDemo(EqualityOperator)/LinqDemo(EqualityOperator)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo_EqualityOperator_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> list1 = new List<string>() { "one", "two", "three", "four", "five" };
+            IList<string> list2 = new List<string>() { "one", "two", "three", "four", "five" };
+            bool isEqual = list1.SequenceEqual(list2);
+            Console.WriteLine(isEqual);
+        }
+    }
+}
