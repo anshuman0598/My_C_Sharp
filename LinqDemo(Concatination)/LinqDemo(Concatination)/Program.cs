ba765ba3d5823ba8359e41c482df0f8fb137faf0
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-10 15:51:29 +0530

    10-03-2022

---
+++ LinqDemo(Concatination)/LinqDemo(Concatination)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo_Concatination_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> list = new List<string>() {"one", "two", "three" };
+            IList<string> list2 = new List<string>() { "four", "five", };
+            var concatList = list.Concat(list2);
+            foreach(var item in concatList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("/------------------------------------------/");
+            IList<int> list3 = new List<int>() { 1, 2, 3 };
+            IList<int> list4 = new List<int>() { 4, 5, 6 };
+            var concatList1 = list3.Concat(list4);
+            foreach (var item in concatList1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+    }
+}
--- LinqDemo(EqualityOperator)/LinqDemo(EqualityOperator)/Program.cs
+++ LinqDemo(EqualityOperator)/LinqDemo(EqualityOperator)/Program.cs
@@ -10,10 +10,33 @@ namespace LinqDemo_EqualityOperator_
     {
         static void Main(string[] args)
         {
-            IList<string> list1 = new List<string>() { "one", "two", "three", "four", "five" };
-            IList<string> list2 = new List<string>() { "one", "two", "three", "four", "five" };
-            bool isEqual = list1.SequenceEqual(list2);
-            Console.WriteLine(isEqual);
+            //IList<string> list1 = new List<string>() { "one", "two", "three", "four", "five" };
+            //IList<string> list2 = new List<string>() { "one", "two", "three", "four", "five" };
+            //bool isEqual = list1.SequenceEqual(list2);
+            //Console.WriteLine(isEqual);
+          
+                Student std = new Student() { StudentId = 1, StudentName = "Bill" };
+                IList<Student> studentList1 = new List<Student>() { std };
+                IList<Student> studentList2 = new List<Student>() { std };
+                bool isStudentEqual = studentList1.SequenceEqual(studentList2);
+                Console.WriteLine(isStudentEqual);
+
+                Student std1 = new Student() { StudentId = 1, StudentName = "Saikat" };
+                Student std2 = new Student() { StudentId = 1, StudentName = "Bill" };
+                IList<Student> studentList3 = new List<Student>() { std1 };
+                IList<Student> studentList4 = new List<Student>() { std2 };
+                isStudentEqual = studentList3.SequenceEqual(studentList4);
+                Console.WriteLine(isStudentEqual);
+                Console.ReadLine();
+            
         }
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string StudentName { get; set; }
+        }
+
     }
+    
+   
 }
