6cb36da674786929d148270a61614598b4934a19
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-10 12:57:19 +0530

    10-03-2022

---
+++ LinqDemo(ElementOperator)/LinqDemo(ElementOperator)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo_ElementOperator_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int>  intList = new List<int>() { 10,20,30,45,50,87 };
+            IList<string> stringList = new List<string>() { "One", "two", null, "four", "five" };
+
+            Console.WriteLine("1st Element in intList " + intList.ElementAt(0));
+            Console.WriteLine("1st Element in stringList " + stringList.ElementAt(0));
+            Console.WriteLine("2nd Element in intList " + intList.ElementAt(1));
+            Console.WriteLine("2nd Element in stringList " + stringList.ElementAt(1));
+            Console.WriteLine("3rd Element in intList " + intList.ElementAtOrDefault(2));
+            Console.WriteLine("3rd Element in stringList " + stringList.ElementAtOrDefault(2));
+            Console.WriteLine("10th Element in intList " + intList.ElementAtOrDefault(9));
+            Console.WriteLine("10th Element in stringList " + stringList.ElementAtOrDefault(9));
+
+            Console.WriteLine("intList.ElementAtOrDefault(9) throws an exception : Index out of range");
+
+            Console.WriteLine(intList.ElementAt(9));
+        }
+    }
+}
