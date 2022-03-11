83f2b0b450c5be0d8816643e3184b3bb9819f661
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-11 09:56:06 +0530

    11-03-2022

---
+++ LinqDemo(Expression)/LinqDemo(Expression)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace LinqDemo_Expression_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<Student, bool>> isTeenAgerExpr = s => s.Age > 12 && s.Age < 20;
+            // Compile Expression using Compile Method to Invoke it as Delegate
+
+            Func<Student, bool> isTeenAger = isTeenAgerExpr.Compile();
+
+            // Invoke
+
+            bool result = isTeenAger(new Student() { StudentID=1, StudentName="John", Age = 15});
+            Console.WriteLine(result);
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
