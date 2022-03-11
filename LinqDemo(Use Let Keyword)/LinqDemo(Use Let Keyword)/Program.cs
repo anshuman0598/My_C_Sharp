3b68519a7caeb85ceb39fae943c6f50e7cbb3e6d
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-11 10:59:00 +0530

    11-03-2022

--- LinqDemo(Use Let Keyword)/LinqDemo(Use Let Keyword)/Program.cs
+++ LinqDemo(Use Let Keyword)/LinqDemo(Use Let Keyword)/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace LinqDemo_Enumerable_ExtensionMethod_
+
+namespace LinqDemo_Use_Let_Keyword_
 {
     internal class Program
     {
@@ -17,26 +18,16 @@ namespace LinqDemo_Enumerable_ExtensionMethod_
                 new Student(){ StudentID = 4, StudentName = "Ram",Age =20},
                 new Student(){ StudentID = 5, StudentName = "Ron",Age =21},
             };
-            var teenAgerStudents = from s in studentList.GetTeenAgerStudents() select s;
-            foreach (var student in teenAgerStudents)
-                Console.WriteLine("Student Name: " + student.StudentName);
+
+            var lowerCaseStudentsNames = from s in studentList
+                                         let lower = s.StudentName.ToLower()
+                                         where lower.StartsWith("r")
+                                         select lower;
+            foreach (var s in lowerCaseStudentsNames)
+                Console.WriteLine("Student Name: " + s);
             Console.Read();
         }
-        
-    }
-    public static class EnumerableExtensionMethods
-    {
-        public static IEnumerable<Student> GetTeenAgerStudents(this IEnumerable<Student> source)
-        {
-            foreach (Student student in source)
-            {
-                Console.WriteLine("Accessing Student: " + student.StudentName);
-                if (student.Age > 12 && student.Age < 20)
-                    yield return student;
 
-            }
-
-        }
     }
     public class Student
     {
@@ -45,5 +36,4 @@ namespace LinqDemo_Enumerable_ExtensionMethod_
         public int Age { get; set; }
     }
 
-    
 }
