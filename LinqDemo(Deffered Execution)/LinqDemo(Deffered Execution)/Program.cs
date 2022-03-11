aa1c3e694a615cc97cd16b18f893afe213828669
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-11 10:28:23 +0530

    11-03-2022

---
+++ LinqDemo(Deffered Execution)/LinqDemo(Deffered Execution)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo_Deffered_Execution_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentList = new List<Student>()
+            {
+                new Student(){ StudentID = 1, StudentName = "John",Age =18},
+                new Student(){ StudentID = 2, StudentName = "Steve",Age =21},
+                new Student(){ StudentID = 3, StudentName = "Bill",Age =18},
+                new Student(){ StudentID = 4, StudentName = "Ram",Age =20},
+                new Student(){ StudentID = 5, StudentName = "Ron",Age =21},
+            };
+            var teenAgerStudents = from s in studentList where s.Age > 12 && s.Age < 20 select s;
+            foreach (var student in teenAgerStudents)
+                Console.WriteLine("Student Name: "+student.StudentName);
+            studentList.Add(new Student() { StudentID = 6, StudentName = "Martin", Age = 16 });
+            Console.WriteLine();
+            foreach (var student in teenAgerStudents)
+                Console.WriteLine("Student Name: "+student.StudentName);
+
+            Console.Read();
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
